caababcbd3279e3ac8652a870f1c6e0ae911a2dd
Kukuh Iksan Musyahada <kukuh.iksan@gmail.com>
2022-06-06 11:24:33 +0700

    intialize path_handling.py

---
+++ essential/path_handling.py
@@ -0,0 +1,38 @@
+import os
+import global_params
+
+case = global_params.CASES
+
+
+def GetDataSource():
+    return return os.path.abspath(
+        os.path.join(GetThisDir(), os.pardir, "Data_source")
+    )
+
+def GetThisDir():
+    return os.path.dirname( os.path.abspath(__file__) )
+
+def GetRawData():
+    return os.path.join(GetDataSource(), "Raw")
+
+def GetProcessedData():
+    return os.path.join(GetDataSource(), "Processed")
+
+def GetModelsData():
+    return os.path.join(GetDataSource(), "Models")
+
+def GetMachVarModel():
+    return os.path.join(GetModelsData(),case[0] )
+
+def GetVFVarModel():
+    return os.path.join(GetModelsData(),case[1] )
+
+def InitDataDirectories():
+    important_dir = global_params.DIRECTORIES
+
+    for dir in important_dir:
+        full_path = os.path.join (GetThisDir(), os.pardir, dir)
+        full_path = os.path.abspath (full_path)
+
+        if not os.path.exists(full_path):
+            os.makedirs(full_path)
