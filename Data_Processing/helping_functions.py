f86bdf6df89481e052349e0efef65dd473c545b7
Kukuh Iksan Musyahada <kukuh.iksan@gmail.com>
2022-06-07 10:27:40 +0700

    initialize helper_functions.py

---
+++ Data_Processing/helping_functions.py
@@ -0,0 +1,68 @@
+import os
+import re
+
+#extracting Mach and Flutter Velocity
+
+def extractMachAndVF(file: str):
+    pattern = r"M_([0-9\.]*)_VF_([0-9\.]*).csv"
+    result = re.match(pattern, file)
+
+    return result.group(1), result.group(2)
+
+# Creating Numpy Dataset
+
+def create_dataset(no_row, no_column, datapath, use_cols):
+    data = []
+    count= 0
+    for file in os.listdir(datapath):
+        if file.endswith(".csv"):
+            df = pd.read_csv(os.path.join(datapath, file), nrows= no_row)
+            df_np = df[use_cols].to_numpy()
+            data.append(df_np)
+            count +=1
+    dataset = np.stack(data, axis=1)
+    dataset = dataset.reshape(no_row, count*no_column)
+    return dataset
+
+# interpolation function
+
+def interpolate(dataset,size_row):
+    x=np.arange(dataset.shape[0])
+    xvals= np.linspace(0,dataset.shape[0],size_row)
+    data_interp= []
+    for col in range(dataset.shape[1]):
+        y = dataset[:,col]
+        yinterp= np.interp(xvals,x,y)
+        data_interp.append(yinterp)
+    data_interp= np.stack(data_interp, axis=1)
+    data_interp.shape
+    
+    return data_interp
+
+# labelling function
+
+def labelling(datapath):
+    labels = []
+    pred_labels= []
+    for file in os.listdir(datapath):
+        if file.endswith(".csv"):
+            MachAndVF= extractMachAndVF(file)
+            label= f'M_{MachAndVF[0]}_VF_{MachAndVF[1]}'
+            labels.append(label)
+            pred_label= f'Pred M_{MachAndVF[0]}_VF_{MachAndVF[1]}'
+            pred_labels.append(pred_label)
+    return labels, pred_labels
+
+# dataframe label
+
+def label_df(Labels:list, no_case:int):    
+    labels=[]
+    cases = case_model[no_case]
+    if no_case == 1 or no_case ==2:
+        for label in Labels:
+            for case in cases:
+                label_name= f'{case}_{label}'
+                labels.append(label_name)
+        return labels
+    else:
+        print('not supported cases!')
