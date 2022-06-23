e16491c39569704bea868866f0c2bfe88f02051f
Kukuh Iksan Musyahada <kukuh.iksan@gmail.com>
2022-06-23 21:30:44 +0700

    add early_stop in models.py

--- Training/models.py
+++ Training/models.py
@@ -9,7 +9,18 @@ from keras.losses import Huber
 from TA_LSTMxFlutter.essential import global_params as gp
 from TA_LSTMxFlutter.essential import path_handling as ph
 
+
 def model(train_data= None,val_data=None,test_data= None,max_epochs=100, num_features= 8):
+    myEarlyStop= tf.keras.callbacks.EarlyStopping(
+    monitor='val_loss',
+    min_delta=1e-5,
+    patience=20,
+    verbose=0,
+    mode='auto',
+    baseline=None,
+    restore_best_weights=True
+)
+
     model = Sequential([
         LSTM(750, input_shape=[gp.LABEL_WIDTH,num_features]),
         Dense(8)
@@ -19,11 +30,11 @@ def model(train_data= None,val_data=None,test_data= None,max_epochs=100, num_fea
             optimizer=tf.keras.optimizers.Adamax(learning_rate= 5e-4),
             metrics=["mae"]
     )
-    model.fit(train_data, epochs=max_epochs, validation_data= val_data,)
+    model.fit(train_data, epochs=max_epochs, validation_data= val_data,callbacks= myEarlyStop)
 
     return model
 
-def save_model(model,no_case= None, no_model= None , vf_case=None, path= ph.GetModelsData):
+def save_model(model,no_case= None, no_model= None , vf_case=None, path= ph.GetModelsData()):
     if no_case==0: 
         names=f'LSTM{no_case}{no_model}.h5'
     elif no_case==1:
