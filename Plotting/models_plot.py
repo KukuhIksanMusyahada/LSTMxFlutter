292d57d39aa4f079740942dc1dfd6fbf9c6199d1
Kukuh Iksan Musyahada <kukuh.iksan@gmail.com>
2022-06-27 08:18:52 +0700

    Add modelTuning.ipnb and still not working models_plot.py

--- Plotting/models_plot.py
+++ Plotting/models_plot.py
@@ -1,8 +1,30 @@
+import matplotlib.pyplot as plt
 
 
+from TA_LSTMxFlutter.essential import path_handling as ph
 
-def history_plot():
-    pass
+def history_plot(history, path = ph.GetModelPerformancesData()):
+  '''Plots the training and validation loss and mae from a history object'''
+  mae = history.history['mae']
+  val_mae = history.history['val_mae']
+  loss = history.history['loss']
+  val_loss = history.history['val_loss']
+  titles= ['Training and validation mae', 'Training and validation loss']
+  epochs = range(len(mae))
+
+  plt.plot(epochs, mae, 'bo', label='Training Mean Absolute Error')
+  plt.plot(epochs, val_mae, 'b', label='Validation Mean Absolute Error')
+  plt.title(titles[0])
+
+  plt.figure()
+
+  plt.plot(epochs, loss, 'bo', label='Training Huber Loss')
+  plt.plot(epochs, val_loss, 'b', label='Validation Huber Loss')
+  plt.title(titles[1])
+  plt.legend()
+
+  plt.show()
+  plt.savefig('.')
 
 def model_forecast_plot():
     pass
