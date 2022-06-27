7afb3adf64fbc5b5110049977d8090ec70f07056
Kukuh Iksan Musyahada <kukuh.iksan@gmail.com>
2022-06-27 23:23:08 +0700

    init runner.py

---
+++ runner.py
@@ -0,0 +1,25 @@
+# Importing Library
+import os
+import re
+import time
+import datetime
+
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import tensorflow as tf
+
+
+from TA_LSTMxFlutter.essential import path_handling as ph
+from TA_LSTMxFlutter.essential import global_params as gp
+from TA_LSTMxFlutter.Data_Processing import data_processing as dp
+from TA_LSTMxFlutter.Data_Processing import training_prep as tp
+from TA_LSTMxFlutter.Training import models
+from TA_LSTMxFlutter.Plotting import models_plot as mp
+
+
+def main():
+    pass
