16cae72131014550ceca096305e480d890e6fbc5
Kukuh Iksan Musyahada <kukuh.iksan@gmail.com>
2022-06-27 23:52:42 +0700

    completed runner.py to make it able to train data

--- runner.py
+++ runner.py
@@ -22,4 +22,42 @@ from TA_LSTMxFlutter.Plotting import models_plot as mp
 
 
 def main():
-    pass
+    start_time= datetime.datetime.now()
+    print(f'LSTMxFlutter Script called at{start_time}')
+
+    # Initialize Dictionary
+    ph.InitDataDirectories()
+
+    #Check whether the model already trained or not?
+    if os.path.isfile(ph.GetModelsData()) == False:
+        # Processing Data
+        dp.data_process()
+
+        # Get dictionary of train_data
+        train_dict= dp.GetDictTrainData()
+
+        # Training
+        for name, train in train_dict.items():
+            print('START TRAINING')
+            df_norm, params = tp.df_norm(train, minmax= True)
+            train_df, val_df, _, num_features = tp.train_val_split(df_norm)
+            print(f'df shape is {train.shape}')
+            print(f'train_df shape is {train_df.shape}')
+            print(f'val_df shape is {val_df.shape}')
+
+            window= tp.WindowGenerator(input_width=1, label_width=1, shift=1,
+                                    train_df = train_df, val_df= val_df,
+                                    label_columns=None, batch_size= 40)
+            train = window.train
+            val = window.val
+
+            features, labels = next(iter(train))
+            print (f'train features shape is {features.shape}')
+            print (f'train labels shape is {labels.shape}')
+
+            val_features, val_labels = next(iter(val))
+            print (f'val features shape is {val_features.shape}')
+            print (f'val labels shape is {val_labels.shape}')
+            
+            model,history = models.model(train_data= train,val_data= val,test_data= None, num_features=num_features)
+            models.save_model(model,names=name)
